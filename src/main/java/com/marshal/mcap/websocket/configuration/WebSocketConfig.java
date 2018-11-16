3053ee9d198d641f72f5e38880f6d56ca24b1767
Marshal1996 <453607106@qq.com>
2018-11-17 01:26:39 +0800

    add by Marshal,websocket基本功能实现

---
+++ src/main/java/com/marshal/mcap/websocket/configuration/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package com.marshal.mcap.websocket.configuration;
+
+import com.marshal.mcap.websocket.handler.WebSocketHandler;
+import com.marshal.mcap.websocket.interceptor.WebSocketHandShakeInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * author: Marshal
+ * Date: 2018/11/15
+ * Time: 20:55
+ * Description:websocket配置类
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+
+        //1.注册WebSocket
+        String websocket_url = "/websocket/socketServer";                        //设置websocket的地址
+        webSocketHandlerRegistry.addHandler(webSocketHandler(), websocket_url).                          //注册Handler
+                addInterceptors(new WebSocketHandShakeInterceptor());                   //注册Interceptor
+
+        //2.注册SockJS，提供SockJS支持(主要是兼容ie8)
+        String sockjs_url = "/sockjs/socketServer";                              //设置sockjs的地址
+        webSocketHandlerRegistry.addHandler(webSocketHandler(), sockjs_url).                            //注册Handler
+                addInterceptors(new WebSocketHandShakeInterceptor()).                   //注册Interceptor
+                withSockJS();
+
+    }
+
+    @Bean
+    public TextWebSocketHandler webSocketHandler() {
+        return new WebSocketHandler();
+    }
+}
